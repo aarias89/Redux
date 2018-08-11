d557500c5349304f3fc6fa306c0e01e93a2bdb7a
[Andres] <andresar11@gmail.com>
2018-08-11 19:32:46 -0400

    Demonstrate app scalability by adding a pages property to book title. Only need to another div to the book detail and call the pages prop.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 //this reducer will only return an Array of objects.
 export default function() {
   return [
-    {title:"Book1-AAAA"},
-    {title:"Book2-BBBB"},
-    {title:"Book3-CCCC"},
-    {title:"Book4-DDDD"},
-    {title:"Book5-EEEE"}
+    {title:"Book1-AAAA", pages: 432},
+    {title:"Book2-BBBB", pages: 102},
+    {title:"Book3-CCCC", pages: 35},
+    {title:"Book4-DDDD", pages: 63},
+    {title:"Book5-EEEE", pages: 694}
   ];
 }
