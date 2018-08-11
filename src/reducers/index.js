782695f8637b151f7f4a30484a23708c24a58870
[Andres] <andresar11@gmail.com>
2018-08-11 19:27:21 -0400

    Add book-detail as a container, since book details will need to actively be interacting with redux state. Connect BookDetail container to redux store by importing connect from react-redux and defining the mapStateToProps function. On book-detail.js add a condition to display a truthy object when this.props.book == null

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
-import ActiveBook from './reducer_active_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
   //this reduce will add a key to our global application state called books
   //where the key is books: and the value is whatever gets returned from
   //the BooksReducer
-  books: BooksReducer
+  books: BooksReducer,
   activeBook: ActiveBook
 });
 
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -2,7 +2,7 @@
 
 //State argument is not refering to the application state, only
 //the state this reducer is responsible for
-export default function(state =  null,action) {
+export default function(state = null,action) {
   switch (action.type) {
     case 'BOOK_SELECTED':
       return action.payload
